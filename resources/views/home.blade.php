96fe360cd98ff44586a7a661e5277f0d00e37041
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-02-19 19:57:48 +0100

    estilos pagina 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
     <div>
-        <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png" alt="">
+        <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png" class= "mx-auto d-block" alt="">
     </div>
-    <h2>¡Hola! Somos la primera comunidad fem con frases de empoderamiento</h2>
-    <span>1<a href="/quotes">Buscar una frase motivadora en nuestro buscador</a></span>
-    <span>2<a href="/login">Únete a nuestra comunidad </a></span>
-    <span>3<p>Guarda tus frases favoritas</p></span>
+    <div class="row align-items-center justify-content-center vh-85 text-center">
+        <h2 class= "text-center">¡Hola! Somos la primera comunidad fem con frases de empoderamiento</h2>
+        <span>1</span>
+            <a href="/quotes">Buscar una frase motivadora en nuestro buscador</a>
+
+        <span>2</span>
+            <a href="/login">Únete a nuestra comunidad </a>
+
+        <span>3</span>
+            <p>Guarda tus frases favoritas</p>
+
+    </div>
     
         <div>
-            <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png" alt="">
+            <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png"  alt="">
             <small>CopyRight 2023</small>
         </div>
     @endsection
