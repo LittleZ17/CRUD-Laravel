d5c67983651ff8d80978791f500b6ac22527a89d
Shima Naderi <shima.naderii@gmail.com>
2023-02-20 13:13:34 +0100

    permission

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,9 @@ use App\Http\Controllers\QuoteController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
@@ -26,6 +24,12 @@ Auth::routes();
  Route::get('/quotes', [QuoteController::class , 'index'])->name('quotes');
  Route::get('/quotes/create', [QuoteController::class, 'create'])->name('create');
  Route::post('/quotes', [QuoteController::class, 'store'])->name('store');
+ 
+ Route::get('/quotes/{id}', [QuoteController::class, 'show'])->name('show');
+ Route::get('/quotes/{id}/edit', [QuoteController::class, 'edit'])->name('edit');
+ Route::put('/quotes/{id}', [QuoteController::class, 'update'])->name('update');
+ Route::delete('/quotes/{id}', [QuoteController::class, 'destroy'])->name('destroy');
+
 
 
 Auth::routes();
