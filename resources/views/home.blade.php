b8a48c7d70cf17f857fae88052c97b32d43807fd
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-02-20 09:41:38 +0100

    stilos footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,14 @@
     </div>
     <div class="row align-items-center justify-content-center vh-85 text-center">
         <h2 class= "text-center">¡Hola! Somos la primera comunidad fem con frases de empoderamiento</h2>
-        <span>1</span>
+        <span style="color:orange;">1</span>
             <a href="/quotes">Buscar una frase motivadora en nuestro buscador</a>
 
-        <span>2</span>
+        <span style="color:orange;">2</span>
             <a href="/login">Únete a nuestra comunidad </a>
 
-        <span>3</span>
+        <span style="color:orange;">3</span>
             <p>Guarda tus frases favoritas</p>
 
     </div>
-    
-        <div>
-            <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png"  alt="">
-            <small>CopyRight 2023</small>
-        </div>
     @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,70 +12,90 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="">
+    {{-- <link rel="stylesheet" href=""> --}}
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('resources/css/app.css') }}" >
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
+
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">QUOTES
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background-color: rgba(90, 168, 151, 1);">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">QUOTES
+                        {{-- {{ config('app.name', 'Laravel') }} --}}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
 
         <main class="py-4">
             @yield('content')
         </main>
+        
+        <footer class="footer fixed-bottom">
+            <div class="pt-3 pb-3 w-full text-center" style="background-color: rgba(90, 168, 151, 1);">
+                <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png"  alt="" height="20px">  <span class="text-white">© 2023 Copyright </span>
+            </div>
+          </footer>
+        <!-- <footer>
+           <div class="flex justify-center bg-gray-800 h-18 pt-4 pb-4 fixed w-full" style="background-color: rgba(90, 168, 151, 1);">
+                 Copyright 
+                <div class="text-center" >
+                    <img src="http://localhost:8888/crud-laravel/assets/quotelogo.png"  alt="" height="20px">  
+                    © 2023 Copyright 
+                </div>-->
+                <!-- Copyright 
+            </div>
+          </footer>-->
     </div>
 </body>
 </html>
