0ebd0a69c92d88a101e716af88c3dc8399ad9fc2
Little Z <zy_mz17@hotmail.es>
2023-02-27 22:14:16 +0100

    slider ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,42 +4,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    {{-- <link rel="stylesheet" href=""> --}}
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('resources/css/app.css') }}" >
-
-    <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
 
     <div id="app">
-        <header>
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" >
-                <div class="container" style="background-color: rgba(90, 168, 151, 1);" >
+        <header style="background-color: rgba(90, 168, 151, 1);">
+            <nav class="navbar navbar-expand-md navbar-light shadow-sm"  >
+                <div class="container"  >
                     <a class="navbar-brand" href="{{ url('/') }}">FEMQUOTE
-                        {{-- {{ config('app.name', 'Laravel') }} --}}
                     </a> 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav me-auto">
 
                         </ul>
 
-                        <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ms-auto">
-                            <!-- Authentication Links -->
                             @guest
                                 @if (Route::has('login'))
                                     <li class="nav-item">
@@ -81,11 +72,7 @@
             @yield('content')
         </main>
         
-        <footer class="footer fixed-bottom">
-            <div class="pt-3 pb-3 w-full text-center" style="background-color: rgba(90, 168, 151, 1);">
-                  <span class="text-white">© Copyright Todos los derechos reservados 2023 || Privacy Policy</span>
-            </div>
-          </footer>
+        {{--  --}}
         
     </div>
 </body>
--- resources/views/quote/index.blade.php
+++ resources/views/quote/index.blade.php
@@ -6,74 +6,73 @@
 
 @section('content')
     <div class="container-fluid">
-       
-            <div class="col-sm-12">
-                {{-- SLIDER VA AQUI!! --}}
-                <div id="carouselExample" class="carousel slide">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="https://images.pexels.com/photos/5874593/pexels-photo-5874593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="https://images.pexels.com/photos/13371882/pexels-photo-13371882.jpeg?auto=compress&cs=tinysrgb&w=1600
-                        " class="d-block w-100" alt="...">
-                      </div>
+
+        <div class="col-sm-12">
+            {{-- SLIDER VA AQUI!! --}}
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://images.pexels.com/photos/5874593/pexels-photo-5874593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            class="d-block w-100" style="height: 300px" alt="...">
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
-                    </button>
-                  </div>
+                    <div class="carousel-item">
+                        <img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            class="d-block w-100" style="height: 300px"" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://images.pexels.com/photos/13371882/pexels-photo-13371882.jpeg?auto=compress&cs=tinysrgb&w=1600
+                        "
+                            class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
 
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center; margin:10px">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center; margin:10px">
 
-                            <span id="card_title">
-                                {{ __('Quote') }}
-                            </span>
-                            @role('admin')
+                        <span id="card_title">
+                            {{ __('Quote') }}
+                        </span>
+                        @role('admin')
                             <div class="float-right">
                                 <a href="{{ route('quotes.create') }}" class="btn btn-primary btn-sm float-right"
                                     data-placement="left">
                                     {{ __('Create New') }}
                                 </a>
                             </div>
-                            @endrole
-                        </div>
+                        @endrole
                     </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-                       
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <div class="col"> 
+                </div>
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                @endif
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="col">
                             @foreach ($quotes as $quote)
-                                <div class="bg-image card shadow-1-strong " 
-                                    style="background-image: url({{ $quote->img}}); 
+                                <div class="bg-image card shadow-1-strong "
+                                    style="background-image: url({{ $quote->img }}); 
                                         backdrop-filter: blur(0px);
                                         background-color: rgba(203,193,209,0.3);}">
-                                    <div class="card-body text-white " >
+                                    <div class="card-body text-white ">
                                         <h5 class="card-title">{{ $quote->prhase }}</h5>
                                         <p class="card-text ">
                                             {{ $quote->author }}
                                         </p>
                                         <div>
-                                                <form action="....." method="POST">
-                                                    <button type="submit" class="btn btn-sm" style="background-color: rgba(90, 168, 151, 1);>
-                                                    <i class="fa fa-fw fa-trash"></i> Añadir a Favoritos</button>
-                                                </form>
-                                            @role('admin')
+                                            @hasallroles('admin')
                                                 <form action="{{ route('quotes.destroy', $quote->id) }}" method="POST">
                                                     <a class="btn btn-sm btn-primary "
                                                         href="{{ route('quotes.show', $quote->id) }}"><i
@@ -81,21 +80,29 @@
                                                     <a class="btn btn-sm btn-success"
                                                         href="{{ route('quotes.edit', $quote->id) }}"><i
                                                             class="fa fa-fw fa-edit"></i> Edit</a>
-                                                    @csrf
-                                                    @method('DELETE')
                                                     <button type="submit" class="btn btn-danger btn-sm"><i
                                                             class="fa fa-fw fa-trash"></i> Delete</button>
                                                 </form>
-                                            @endrole
+                                            @else
+                                                <form action="....." method="POST">
+                                                    <button type="submit" class="btn btn-sm"
+                                                        style="background-color: rgba(90, 168, 151, 1)";>
+                                                        <i class="fa fa-fw fa-trash"></i> Añadir a Favoritos</button>
+                                                </form>
+                                            @endhasallroles
                                         </div>
                                     </div>
-                                </div>    
+                                </div>
                             @endforeach
                         </div>
                     </div>
                 </div>
-                {!! $quotes->links() !!}
             </div>
         </div>
     </div>
 @endsection
+<footer class="footer fixed-bottom">
+    <div class="pt-3 pb-3 w-full text-center" style="background-color: rgba(90, 168, 151, 1);">
+          <span class="text-white">© Copyright Todos los derechos reservados 2023 || Privacy Policy</span>
+    </div>
+  </footer>
